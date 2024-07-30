b24092d9e64a9efb2d3c8a31c986dd74558abdfe
kingsusuputih <harsa.aditya.ha@gmail.com>
2024-07-30 11:54:30 +0700

    first commit

---
+++ Sela copy.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Vendor\Kingsusuputih\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
+
+class Sela
+{
+    private $sela_domain;
+    private $sela_kode;
+    private $bearer_token;
+
+    const SELA_URL = 'http://192.168.1.24:8000/api/lisensi-sela/cek';
+    const TOKEN_URL = 'http://192.168.1.24:8000/api/login';
+
+    public function __construct()
+    {
+        $this->sela_domain = $this->getSelaDomain();
+        $this->sela_kode = env('SELA_KODE', 'weymalingpangsit');
+
+        if (!$this->sela_domain || !$this->sela_kode) {
+            return $this->renderError('Sela License details are not set correctly');
+        }
+
+        $this->bearer_token = $this->getBearerToken();
+        if (!$this->bearer_token) {
+            return $this->renderError('Failed to retrieve Bearer Token');
+        }
+
+        return $this->autoCheck();
+    }
+
+    private function getSelaDomain()
+    {
+        $host = $_SERVER['HTTP_HOST'];
+        return (substr($host, 0, 4) === "www.") ? substr($host, 4) : $host;
+    }
+
+    private function getBearerToken()
+    {
+        $client = app(Client::class);
+
+        try {
+            $response = $client->post(self::TOKEN_URL, [
+                'json' => [
+                    'email' => 'api@sevenlight.id',
+                    'password' => '@Sevel2024',
+                ],
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Content-Type' => 'application/json',
+                ],
+                'verify' => true,
+            ]);
+
+            $data = json_decode($response->getBody()->getContents(), true);
+
+            return $data['access_token'] ?? null;
+        } catch (\Exception $e) {
+            Log::error('Failed to retrieve Bearer Token: ' . $e->getMessage());
+            return null;
+        }
+    }
+
+    private function autoCheck()
+    {
+        $client = app(Client::class);
+        $params = [
+            'domain' => $this->sela_domain,
+            'kode' => $this->sela_kode,
+        ];
+
+        try {
+            $response = $client->post(self::SELA_URL, [
+                'json' => $params,
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->bearer_token,
+                    'Accept' => 'application/json',
+                    'Content-Type' => 'application/json',
+                ],
+                'verify' => true,
+            ]);
+
+            $data = json_decode($response->getBody()->getContents(), true);
+
+            if (isset($data['status']) && $data['status'] == 'error') {
+                return $this->renderError($data['pesan']);
+            }
+        } catch (\Exception $e) {
+            Log::error('Sela CMS License Check Failed: ' . $e->getMessage());
+            return $this->renderError('Sela CMS License Check Failed');
+        }
+    }
+
+    private function renderError($message)
+    {
+        echo view('lisensi-sela', ['pesan' => $message]);
+        die;
+    }
+}
---
+++ views/lisensi-sela.blade.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable" data-theme="interactive"
+  data-theme-colors="default" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+
+  <meta charset="utf-8" />
+  <title>Lisensi | {{ env('APP_NAME') }}</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Sela CMS adalah content management sistem yang ringan, cepat, dan modern" />
+  <meta name="author" content="Sevenlight.ID" />
+  <!-- App favicon -->
+  <link href="{{ url('themes/backend') }}/assets/images/favicon.ico" rel="shortcut icon">
+
+  <!-- Layout config Js -->
+  <script src="{{ url('themes/backend') }}/assets/js/layout.js"></script>
+  <!-- Bootstrap Css -->
+  <link type="text/css" href="{{ url('themes/backend') }}/assets/css/bootstrap.min.css" rel="stylesheet" />
+  <!-- Icons Css -->
+  <link type="text/css" href="{{ url('themes/backend') }}/assets/css/icons.min.css" rel="stylesheet" />
+  <!-- App Css-->
+  <link type="text/css" href="{{ url('themes/backend') }}/assets/css/app.min.css" rel="stylesheet" />
+  <!-- custom Css-->
+  <link type="text/css" href="{{ url('themes/backend') }}/assets/css/custom.min.css" rel="stylesheet" />
+
+  <style>
+    body::before {
+      opacity: 100% !important;
+    }
+
+    body {
+      background: url('{{ url('assets/defaults/gambar/lisensi.png') }}');
+      background-size: cover;
+      background-position: top;
+      background-repeat: no-repeat;
+    }
+  </style>
+
+</head>
+
+<body>
+
+  <div class="auth-page-wrapper d-flex justify-content-center align-items-end vh-100 bg-white py-5 opacity-75">
+
+    <div class="auth-page-content overflow-hidden p-0">
+      <div class="container">
+
+        <div class="row justify-content-center">
+          <div class="col-xl-7 col-lg-8">
+
+            <div class="text-center">
+
+              {{-- <img src="{{ url('assets/defaults/gambar/lisensi.png') }}" alt="" class="w-100 object-fit-contain" style="height: 50vh;"> --}}
+
+              <h3 class="text-uppercase fw-bold text-dark">{{ $pesan }}</h3>
+              <p class="text-dark mb-0">
+                {{-- Maaf, kamu tidak memiliki akses ke halaman <b class="text-dark">{{ url()->current() }}</b> <br> --}}
+                Silahkan hubungi <a href="https://sevenlight.id">Sevenlight.ID</a> untuk info lebih lanjut.
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  </div>
+
+</body>
+
+</html>
