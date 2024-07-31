666ef06f9f64f4bb24bde9e317402415964248fe
kingsusuputih <harsa.aditya.ha@gmail.com>
2024-07-31 17:24:11 +0700

    update lg

--- src/SelaLisensiServiceProvider.php
+++ src/SelaLisensiServiceProvider.php
@@ -4,21 +4,16 @@ namespace Kingsusuputih\LisensiSela;
 
 use Illuminate\Support\ServiceProvider;
 
-class SelaServiceProvider extends ServiceProvider
+class SelaLisensiServiceProvider extends ServiceProvider
 {
     public function register()
     {
         // Binding dan register service di sini
+        new Sela();
     }
 
     public function boot()
     {
         // Bootstrap service di sini
-    }
-
-    // Tambahkan metode isDeferred jika diperlukan
-    public function isDeferred()
-    {
-        return false;
     }
 }
