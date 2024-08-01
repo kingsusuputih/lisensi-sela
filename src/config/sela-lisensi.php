aac43846c4849164755f815c5a95f6c42b81043d
kingsusuputih <harsa.aditya.ha@gmail.com>
2024-08-01 10:49:31 +0700

    perubahan config

---
+++ src/config/sela-lisensi.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'version' => 'v-zero-one',
+];
--- src/config/view.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'paths' => [
-        resource_path('views'),
-        base_path('vendor/kingsusuputih/lisensi-sela/src/resources/views'),
-    ],
-];
